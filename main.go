413056ea44170d3ca21414cac61b9d49832102cf
cxguyton <cxguyton@gmail.com>
2026-08-29 09:16:37 -0500

    Cleaned Routes added Healthz

--- main.go
+++ main.go
@@ -4,10 +4,17 @@ import (
 	"net/http"
 )
 
+func handleHealthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/",http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", handleHealthz)
 
 	http.ListenAndServe("localhost:8080", mux)
 }
